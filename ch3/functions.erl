abd17197b4002013a155ebbb93623ffc4e3ba743
kamalogudah <mcpaul2058@gmail.com>
2019-10-19 14:10:03 +0300

    Guard Clauses

--- ch3/functions.erl
+++ ch3/functions.erl
@@ -1,5 +1,5 @@
 - module(functions).
-- export([head/1, second/1, same/2,valid_time/1]).
+- export([head/1, second/1, same/2,valid_time/1, old_enough/1, right_age/1, wrong_age/1]).
 head([H|_]) -> H.
 second([_,X|_]) -> X.
 same(X,X) ->
@@ -12,4 +12,20 @@ valid_time({Date = {Y, M,D}, Time = {H, Min, S}}) ->
 
 valid_time(_) ->
     io:format("Stop feeding me wrong data! ~n").
+
+%% Guards
+old_enough(X) when X >= 16 -> true;
+old_enough(_) ->
+  false.
+
+right_age(X) when X >= 16, X =< 104 ->
+  true;
+right_age(_) ->
+  false.
+
+wrong_age(X) when X < 16; X > 104 ->
+  true;
+
+wrong_age(_) ->
+  false.
 
