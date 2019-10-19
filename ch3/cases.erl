7659c65a58e994973ed741a21135a73574647c64
kamalogudah <mcpaul2058@gmail.com>
2019-10-19 23:59:30 +0300

    Case statement

---
+++ ch3/cases.erl
@@ -0,0 +1,21 @@
+-module(cases).
+-export([insert/2, beach/1]).
+insert(X,[]) ->
+  [X];
+insert(X,Set) ->
+  case lists:member(X,Set) of
+    true -> Set;
+    false -> [X|Set]
+end.
+
+beach(Temperature) ->
+  case Temperature of
+    {celsius, N} when N >= 20, N =< 45 ->
+      'favourable';
+    {kelvin, N} when N >= 293, N =< 318 ->
+      'scientifically favourable';
+    {fahrenheit, N} when N >= 68, N =< 113 ->
+      'favourable in the US';
+    _ ->
+         'avoid beach'
+end.
