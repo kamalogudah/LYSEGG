ca510f69c27f9500f4877db523d3082ccfbbf579
kamalogudah <mcpaul2058@gmail.com>
2019-10-19 13:35:44 +0300

    Variables in a bind

--- ch3/functions.erl
+++ ch3/functions.erl
@@ -1,8 +1,15 @@
 - module(functions).
-- export([head/1, second/1, same/2]).
+- export([head/1, second/1, same/2,valid_time/1]).
 head([H|_]) -> H.
 second([_,X|_]) -> X.
 same(X,X) ->
     true;
 same(_X,_Y) ->
     false.
+valid_time({Date = {Y, M,D}, Time = {H, Min, S}}) ->
+    io:format("The Date tuple (~p) says today is: ~p/~p/~p, ~n", [Date, Y,M,D]),
+    io:format("The Time tuple (~p) indicates : ~p:~p:~p. ~n", [Time, H,Min,S]);
+
+valid_time(_) ->
+    io:format("Stop feeding me wrong data! ~n").
+
